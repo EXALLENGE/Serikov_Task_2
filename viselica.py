87e56102c880409648f863acaa500c129673081f
TTgrey <sasha6615r@mail.ru>
2019-02-24 17:00:21 +0300

    fix

---
+++ viselica.py
@@ -0,0 +1,20 @@
+import random
+
+words = ['random', 'fastest', 'cool', 'task', 'visible']
+word = random.choice(words)
+health = 5
+letters = [i for i in word]
+alletters='abcdefghijklmnopqrstuvwxyz'
+while i for i in alletters != letter or len(letter) > 2:
+    letter = input('guess a letter:')
+
+
+
+
+def answer (letter):
+
+
+    return
+
+
+
